8728cb925942c1bc1f546952ce894aaba2b2cc47
Simeon Dodov <s.dodov@outlook.com>
2025-02-13 18:56:55 +0200

    server should serve static files

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -54,7 +54,7 @@ export const loginUser = async (email: string, password: string) => {
   const { password: userPassword, ...safeUserData } = user;
 
   const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "24h",
+    expiresIn: "60d",
   });
 
   return { token, user: safeUserData };
