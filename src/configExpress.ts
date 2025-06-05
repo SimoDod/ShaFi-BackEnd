ec714311e151c3066d877fe3d24caf8682a04b74
Simeon Dodov <s.dodov@outlook.com>
2025-06-05 11:49:23 +0300

    remove rate limiter

--- src/configExpress.ts
+++ src/configExpress.ts
@@ -14,11 +14,6 @@ import path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 300 requests per 15 minutes limit
-  max: 300,
-  message: errMsg.tooManyRequests,
-});
 
 const configExpress = (app: Application) => {
   app.options("*", cors());
@@ -51,7 +46,6 @@ const configExpress = (app: Application) => {
   );
   app.use(bodyParser.json());
   app.use(e.static(path.join(__dirname, "../../distStatic")));
-  app.use(limiter);
   app.use("/api", routes);
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../../distStatic", "index.html"));
